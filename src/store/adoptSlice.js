80a40814266800001de24ffeaff70ea4fbdafbe1
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-07-26 23:53:17 +0500

    contract reset with two admins and reinvest removed

--- src/store/adoptSlice.js
+++ src/store/adoptSlice.js
@@ -22,6 +22,13 @@ const abi = [
 	},
 	{
 		"inputs": [],
+		"name": "redeemBNBs",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
 	},
@@ -43,38 +50,6 @@ const abi = [
 		],
 		"name": "Buy",
 		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "referrer",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "buyInvest",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "redeemBNBs",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "reinvest",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
 	},
 	{
 		"inputs": [
@@ -672,7 +647,7 @@ export const initWeb3 = createAsyncThunk(
 			
                 await Web3.givenProvider.enable()
                 const networkId = await web3.eth.net.getId()
-				const SeekGoldAddress = "0x8c37309Da7e74509755ADD96bF9CF1531a9cb850"
+				const SeekGoldAddress = "0x170de387bf449AA93e80981e8a9057a00d3E75Ad"
 				var contract = new web3.eth.Contract(abi, SeekGoldAddress);
                 
 				SeekGoldContract = contract;
